8cbe6441b0414c77a3f97b3cd49bdcd35357cf9a
dabrencreationz <brenvaltessy@yahoo.com>
2023-07-06 14:01:51 +0100

    type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+//type annotation - function to add two floats
+
+def add(a: float,b: float) -> float:
+    return a + b
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+//basic annotations - change to string 
+
+
+def to_str(n: float) -> str:
+    return str(n)
